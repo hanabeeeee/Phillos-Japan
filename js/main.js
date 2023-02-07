d81a4eb66c4b46639f43831e8df346396daa1b7d
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2023-02-07 15:00:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+    // 스크롤 시 header fade-in
+    $(document).on('scroll', function(){
+        if($(window).scrollTop() > 1000){
+            $("#gotop_wrap").removeClass("deactive");
+            $("#gotop_wrap").addClass("active");
+        }else{
+            $("#gotop_wrap").removeClass("active");
+            $("#gotop_wrap").addClass("deactive");
+        }
+    })
+});
+
+
+$(document).ready(function(){
+    clientW = $("body").width();
+
+   if(clientW == 1180){
+      $(".gotop_box").css("width","150px");
+      $(".gotop_box").css("height","150px");
+      $(".gotop_box img").css("width","100px");
+      $(".gotop_box i").css("font-size","100px");
+   }
+});
+  
